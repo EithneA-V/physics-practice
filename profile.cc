7d1f10e9890e8093c532345bfb5be21315e5f427
EithneA-V <eithne.arsuaga.vazquez@gmail.com>
2019-05-21 21:19:46 -0700

    Finish GetTime()

--- profile.cc
+++ profile.cc
@@ -6,16 +6,24 @@ const double Profile::GetTime() {
   double VelocityInitial = 0.0;
   double t1;
   double t2;
+  double d1;
+  double d2;
   double TimeTotal;
 
   if (UsingMaxVelocity) {
+
   t1 = (kMaxVelocity - VelocityInitial)/kMaxAcceleration;
-  t2 = 1 - t1;
-  TimeTotal = (t1*2) + (t2);
+  d1 = t1*kMaxVelocity; //area of two triangles (increasing and decreasing velocity)
+  d2 = goal_ - d1; //area of rectangle
+  t2 = d2/kMaxVelocity;
+  TimeTotal = (2*t1) + t2;
   return TimeTotal;
+
   } else {
+
   t1 = sqrt(2*goal_/kMaxAcceleration);
   return t1;
+
   }
 
   return 0.0;
